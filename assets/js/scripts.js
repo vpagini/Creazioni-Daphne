5769166689fb12bf79f97a20f2bd89083fcefc5d
Veronica Pagini <veronica.pagini@outlook.com>
2016-03-12 16:44:44 +0100

    new albums!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -210,7 +210,18 @@ jQuery(window).load(function() {
 			}
 		}
 	});
-	
+    //events galleries
+   $('.newGal').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+        delegate: 'a', // the selector for gallery item
+        type: 'image',
+        gallery: {
+          enabled:true,
+          navigateByImgClick: true,
+          preload: [0,1] 
+        }
+    });
+});
 	/*
 		Hidden images
 	*/
